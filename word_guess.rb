90d507f915ab8cf762b8966df3ffcca96357a436
ammyy9908 <sb78639@gmail.com>
2022-02-08 18:51:51 +0530

    word guess modified

--- word_guess.rb
+++ word_guess.rb
@@ -1,9 +1,22 @@
 secret_word = "star"
 guess = ""
-
-while guess!=secret_word
-  print "Enter a guess: "
-  guess = gets.chomp()
+chance = 0
+total_chance = 5
+out_of_guess = false
+while guess!=secret_word and !out_of_guess
+    if chance < total_chance
+        print "Enter a guess: "
+         guess = gets.chomp()
+        chance+=1
+    else
+        out_of_guess = true
+        break
+    end
+  
 end
 
-puts "You guessed the secret word!"
+if out_of_guess
+    puts "You are out of guess"
+else
+    puts "You win"
+end
