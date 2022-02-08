4abc548a15e65e519be9de77f789176b49f1d8d4
ammyy9908 <sb78639@gmail.com>
2022-02-08 19:56:18 +0530

    comments added

---
+++ comments.rb
@@ -0,0 +1,7 @@
+# comments-un executable code to just illsutrate the user who want to read your code
+
+# This is a Comment in Ruby
+
+# print "Hello World"
+
+puts "The above print line is not executable by ruby"
