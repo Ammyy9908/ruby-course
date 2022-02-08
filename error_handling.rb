86d94c7462747dd4813c2f4ce20a57ac23f66c33
ammyy9908 <sb78639@gmail.com>
2022-02-08 22:31:57 +0530

    error handling

---
+++ error_handling.rb
@@ -0,0 +1,15 @@
+#num = 10/0 # divide by zero error
+
+
+
+begin
+ lucky_nums = [1,12,32]
+ lucky_nums["dog"]
+rescue ZeroDivisionError
+    puts "Divide by Zero Error"
+rescue TypeError=>e
+    puts e
+end
+
+
+    
