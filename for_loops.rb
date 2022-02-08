47ae7f89ae7903d67e0f043b7de09f91484fffc1
ammyy9908 <sb78639@gmail.com>
2022-02-08 19:49:13 +0530

    for loops added

---
+++ for_loops.rb
@@ -0,0 +1,17 @@
+friends = ["Sumit","Rahul","Shravran","Junaid"]
+
+for friend in friends
+    puts friend
+end
+
+friends.each do |friend|
+    puts friend
+end
+
+for index in 0..5
+    puts index
+end
+
+10.times do |index|
+    puts index
+end
